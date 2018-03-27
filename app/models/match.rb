a943b841509763acee439fe7e8e50829b8ec8fbb
Mauricio Guzmán <mauricio.guzman.salazar@gmail.com>
2018-03-27 14:45:32 -0500

    finishes seed for the first group

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,6 +1,6 @@
 class Match < ApplicationRecord
-  belongs_to :home_team
-  belongs_to :away_team
+  belongs_to :home_team, class_name: 'Team'
+  belongs_to :away_team, class_name: 'Team'
   belongs_to :stadium
   belongs_to :group
 end
